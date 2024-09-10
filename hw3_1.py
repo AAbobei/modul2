c88351e92d740e6c01b1261b9db06bebcc716b45
nout <tazasho47@gmail.com>
2024-09-10 20:31:53 +0300

    add new homework

--- hw3_1.py
+++ hw3_1.py
@@ -1,11 +1,27 @@
 calls = 0
 
-def count_calls(calls):
+def count_calls():
+    global calls
     calls += 1
     return calls
 
 def string_info(string):
-    a = (len(string), string.lower(), string.upper)
+    a = (len(string), string.lower(), string.upper())
+    count_calls()
     return a
 
-print(string_info(string))
+def is_contains(string, list_to_search):
+    count_calls()
+    for i in range(len(list_to_search)):
+        list_to_search[i] = list_to_search[i].upper()
+    if string.upper() in list_to_search:
+        return True
+    else:
+        return False
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(string_info('AdfwerfSAEF'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
+print(is_contains('cycle', ['recycling', 'cyclic']))
+print(calls)
