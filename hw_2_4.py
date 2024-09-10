7e4420713e2616f9cd1584d5a5611019e8fa2568
vadyga <44578613+vadyga@users.noreply.github.com>
2024-09-11 00:08:25 +0300

    Add new homework

--- hw_2_4.py
+++ hw_2_4.py
@@ -14,3 +14,18 @@ for i in range(len(numbers)):
         primes.append(numbers[i])
 print(primes)
 print(not_primes)
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+primes = []
+not_primes = []
+for i in range(len(numbers)):
+    if numbers[i] == 1:
+        continue
+    j = 2
+    for j in range(2, numbers[i]):
+        if numbers[i] % j == 0:
+            not_primes.append(numbers[i])
+        if numbers[i] not in not_primes:
+            primes.append(numbers[i])
+print(primes)
+print(not_primes)
