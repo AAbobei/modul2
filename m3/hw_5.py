95a077bffd4165a0799d14ee61a95edce831802f
vadyga <44578613+vadyga@users.noreply.github.com>
2024-09-21 15:35:04 +0300

    Add new homework

--- m3/hw_5.py
+++ m3/hw_5.py
@@ -2,11 +2,14 @@ def get_multiplied_digits(number):
     str_number = str(number)
     first = 1
     if len(str_number) == 1:
+        if int(str_number) == 0:
+            return first
         first = int(str_number)
         return first
     else:
         first = int(str_number[0])
         return first * get_multiplied_digits(int(str_number[1:]))
 
-result = get_multiplied_digits(40203)
+
+result = get_multiplied_digits(420)
 print(result)
