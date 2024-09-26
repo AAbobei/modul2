313f0fcec4d86f5d07114e8e826f33952c746f9e
HD <tazasho47@gmail.com>
2024-09-26 20:00:25 +0300

    add new homework

--- m5/hw_1.py
+++ m5/hw_1.py
@@ -17,6 +17,65 @@ class House:
             for i in range(a):
                 print(i + 1)
 
+    def __eq__(self, other):
+        if self.number_of_floors == other:
+            return True
+        else:
+            return False
+
+    def __lt__(self, other):
+        if self.number_of_floors < other:
+            return True
+        else:
+            return False
+
+    def __le__(self, other):
+        if self.number_of_floors <= other:
+            return True
+        else:
+            return False
+
+    def __gt__(self, other):
+        if self.number_of_floors > other:
+            return True
+        else:
+            return False
+
+    def __ge__(self, other):
+        if self.number_of_floors >= other:
+            return True
+        else:
+            return False
+
+    def __ne__(self, other):
+        if self.number_of_floors != other:
+            return True
+        else:
+            return False
+
+    def __add__(self, other):
+        if isinstance(other, int):
+            return f'Назавание: {self.name}, колличество этажей: {self.number_of_floors + other}'
+        else:
+            return ('Не цисло')
+
+    def __radd__(self, other):
+        if isinstance(other, int):
+            return f'Назавание: {self.name}, колличество этажей: {self.number_of_floors + other}'
+        else:
+            return ('Не цисло')
+
+    def __iadd__(self, other):
+        if isinstance(other, House):
+            self.number_of_floors += other.number_of_floors
+            return self#f'Назавание: {self.name}, колличество этажей: {self.number_of_floors + other}'
+        if isinstance(other, int):
+            self.number_of_floors += other
+            return self
+        else:
+            return ('Не цисло')
+
+
 house = House('ЖК Эльбрус', 30)
 house.go_to(45)
 house.go_to(0)
@@ -29,3 +88,21 @@ print(len(h2))
 
 print(h1)
 print(h2)
+
+print(h1 == h2) # __eq__
+
+h1 = h1 + 10 # __add__
+print(h1)
+print(h1 == h2)
+
+h1 += 10 # __iadd__
+print(h1)
+
+h2 = 10 + h2 # __radd__
+print(h2)
+
+print(h1 > h2) # __gt__
+print(h1 >= h2) # __ge__
+print(h1 < h2) # __lt__
+print(h1 <= h2) # __le__
+print(h1 != h2) # __ne__
